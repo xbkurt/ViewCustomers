ddb5d394605aabb673565bba5887afd05861e49b
xbkurt <44254696+xbkurt@users.noreply.github.com>
2020-10-04 20:15:07 +0300

    Project installation and init files creation
    React Native application created.
    React-redux, panresponder, react-native-gesture-handler and re-animated libraries installed.
    Index.js and App.js created

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import {store} from './store';
+import {Provider} from 'react-redux';
+import {App} from './app/index';
+
+const Root = () => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
+
+export default Root;
